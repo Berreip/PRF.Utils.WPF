830e59e09d3eedcbb630d823744eba0cc9db7e0f
pierre bony <pierre.bony@hotmail.fr>
2019-02-28 22:35:44 +0100

    Ajout BootStrapper Presentation et NotifierBase

---
+++ PRF.Utils.WPF.UnitTest/NotifyTest/NotifyTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PRF.Utils.WPF.UnitTest.NotifyTest
+{
+    public class TestNotifierBaseClass : NotifierBase
+    {
+        private int _property;
+
+        public int Property
+        {
+            get => _property;
+            set
+            {
+                _property = value;
+                Notify();
+            }
+        }
+    }
+
+    [TestClass]
+    public class NotifyTest
+    {
+        private TestNotifierBaseClass _instance;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // mock:
+
+            // instance de test:
+            _instance = new TestNotifierBaseClass();
+        }
+
+        /// <summary>
+        /// Cas 1: test performance
+        /// </summary>
+        [TestMethod]
+        public void NotifyV1()
+        {
+            //Configuration
+            const int upper = 5_000_000;
+
+            //Test
+            var watch = Stopwatch.StartNew();
+            for (int i = 0; i < upper; i++)
+            {
+                _instance.Property++;
+            }
+            watch.Stop();
+
+            //Verify
+            Assert.IsTrue(watch.Elapsed < TimeSpan.FromMilliseconds(500), $"trop long: {watch.ElapsedMilliseconds} ms");
+            
+        }
+
+
+    }
+}
---
+++ PRF.Utils.WPF/NotifierBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using PRF.Utils.CoreComponents.Extensions;
+
+namespace PRF.Utils.WPF
+{
+    /// <summary>
+    /// Classe de base pour toutes les classes notifiante. 
+    /// Afin d'eviter les fuites mémoires, il est conseillé de dériver de cette classe dans tous les ViewModel et tous les adapteurs
+    /// (ou d'implémenter INotifyPropertyChanged)
+    /// </summary>
+    public abstract class NotifierBase : INotifyPropertyChanged
+    {
+        private readonly HashSet<string> _propertyHash;
+
+        /// <inheritdoc />
+        /// <summary>
+        /// L'évènement de changement des propriétés notifiantes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        protected NotifierBase()
+        {
+            // stocke les propriétés à la construction pour la vérification:
+            _propertyHash = GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Select(o => o.Name)
+                .ToHashSet();
+        }
+
+        /// <summary>
+        /// Notifie un changement de valeur d'une propriété notifiable
+        /// </summary>
+        protected void Notify([CallerMemberName] string propertyName = null)
+        {
+            if (!_propertyHash.Contains(propertyName))
+            {
+                throw new InvalidOperationException($"Trying to raise notification on property \"{propertyName}\" which does not exists on type \"{GetType().Name}\"");
+            }
+            InvokeProperty(new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void InvokeProperty(PropertyChangedEventArgs e)
+        {
+            var handler = PropertyChanged;
+            try
+            {
+                handler?.Invoke(this, e);
+            }
+            catch
+            {
+                //uniquement pour prévenir les rares cas de plantage (bug du framework) en cas de notification d'une propriété mise à jour de puis un thread de background 
+                //et surveillée par le liveshapping d'une collectionview 
+                try
+                {
+                    //du coup on re-essaye:
+                    handler?.Invoke(this, e);
+                }
+                catch
+                {
+                    //et on avale si ca replante une 2e fois
+                }
+            }
+        }
+    }
+}
