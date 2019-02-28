5a4fbc3b0886c5f279826718a841b0a63529e1cb
pierre bony <pierre.bony@hotmail.fr>
2019-02-28 23:02:58 +0100

    Ajout d'un gestionnaire de popup

---
+++ PRF.Utils.WPF/PopupManager/IPopupWindowsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PRF.Utils.WPF.PopupManager
+{
+    /// <summary>
+    /// Interface qui identifie un viewModel de popup, chaque fenêtre de popup doit l'implémenter
+    /// </summary>
+    public interface IPopupWindowsViewModel
+    {
+        /// <summary>
+        /// Méthode appelée lors de la fermeture de la fenêtre
+        /// </summary>
+        void OnClose();
+    }
+}
