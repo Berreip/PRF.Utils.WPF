85b259fbf8f23e11c4277b92be653a6e6ea8649b
Berreip <pierre.bony@hotmail.fr>
2021-11-24 22:48:26 +0100

    Ajout debug assert et fail dans écran

--- WpfModelApp.WPFCore/Views/SecondaryView/SecondaryView1/Secondary1ViewModel.cs
+++ WpfModelApp.WPFCore/Views/SecondaryView/SecondaryView1/Secondary1ViewModel.cs
@@ -1,8 +1,29 @@
-﻿using PRF.WPFCore;
+﻿using System.Diagnostics;
+using PRF.WPFCore;
+using PRF.WPFCore.Commands;
 
 namespace WpfModelApp.WPFCore.Views.SecondaryView.SecondaryView1
 {
     internal class Secondary1ViewModel : ViewModelBase
     {
+        public IDelegateCommandLight DebugAssertCommand { get; }
+        public IDelegateCommandLight DebugFailCommand { get; }
+
+        public Secondary1ViewModel()
+        {
+            DebugAssertCommand = new DelegateCommandLight(ExecuteDebugAssertCommand);
+            DebugFailCommand = new DelegateCommandLight(ExecuteDebugFailCommand);
+
+        }
+
+        private void ExecuteDebugFailCommand()
+        {
+            Debug.Fail("Manual debug fail");
+        }
+
+        private void ExecuteDebugAssertCommand()
+        {
+            Debug.Assert(this.GetType() == typeof(IDelegateCommandLight), "expected type is not expected");
+        }
     }
 }
